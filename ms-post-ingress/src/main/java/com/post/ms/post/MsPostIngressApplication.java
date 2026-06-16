c6ae016bb66fa1a0fddd05eb1d8982c3aedb443f
Mamedli Nuran <mamedlinuran0006@mail.ru>
2026-06-16 22:36:16 +0400

    init

---
+++ ms-post-ingress/src/main/java/com/post/ms/post/MsPostIngressApplication.java
@@ -0,0 +1,13 @@
+package com.post.ms.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPostIngressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPostIngressApplication.class, args);
+    }
+
+}
---
+++ ms-post-ingress/src/test/java/com/post/ms/post/MsPostIngressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post.ms.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPostIngressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
