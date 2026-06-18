e6ceef67253278b58ac9e9f7ec57e061c6fbafd6
Mamedli Nuran <mamedlinuran0006@mail.ru>
2026-06-18 15:40:55 +0400

    fix bug with default value of url in feign client

--- ms-post-ingress/src/main/java/com/post/ms/post/client/MetaFeignClient.java
+++ ms-post-ingress/src/main/java/com/post/ms/post/client/MetaFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "metaGraphClient",
-        url = "${meta.api.base-url}",
+        url = "${meta.api.base-url:https://graph.facebook.com/v25.0}",
         configuration = FeignClientConfig.class
 )
 public interface MetaFeignClient {
