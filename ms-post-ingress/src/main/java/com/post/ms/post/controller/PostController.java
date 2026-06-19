3f6f6953f3f6068f8ae13ff938296a6c34cb2318
Nuran Mamedli <nuran.mammadli.anar2@bsu.edu.az>
2026-06-19 22:30:35 +0400

    Implement privacy policy endpoint
    Added a new endpoint for the privacy policy.

--- ms-post-ingress/src/main/java/com/post/ms/post/controller/PostController.java
+++ ms-post-ingress/src/main/java/com/post/ms/post/controller/PostController.java
@@ -42,4 +42,16 @@ public class PostController {
                 .status(HttpStatus.OK)
                 .body(postService.getBestDay());
     }
+
+    @GetMapping("/privacy")
+    public String privacy() {
+    return """
+        Privacy Policy
+        
+        This application is created for educational purposes.
+        No personal data is stored or shared with third parties.
+        Data received from Meta APIs is used only for demonstration and analysis.
+        Contact: your-email@example.com
+        """;
+    }
 }
