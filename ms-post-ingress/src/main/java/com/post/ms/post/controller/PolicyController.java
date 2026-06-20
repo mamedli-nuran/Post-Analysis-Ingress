e14a7b3fa26b08a9eab5487fcef5ad0be9bf7fda
Mamedli Nuran <mamedlinuran0006@mail.ru>
2026-06-20 12:39:20 +0400

    fix bug with parse data and time 2

--- ms-post-ingress/src/main/java/com/post/ms/post/controller/PolicyController.java
+++ ms-post-ingress/src/main/java/com/post/ms/post/controller/PolicyController.java
@@ -1,9 +1,11 @@
 package com.post.ms.post.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/privacy")
+@RestController
+@RequestMapping("/api/v1/privacy")
 public class PolicyController {
 
     @GetMapping
